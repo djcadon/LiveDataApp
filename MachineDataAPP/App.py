e28c87bf4b62d0a2a8b95b80421b394bd0e9a6b0
djcadon <caden1215@ipmolders.com>
2024-08-14 14:23:25 -0400

    changed to a stacked widget and now working on requests and sockets

--- MachineDataAPP/App.py
+++ MachineDataAPP/App.py
@@ -1,59 +1,101 @@
+#Standard Libraries
 import json
+import os
+#External Libraries
 from PyQt6 import uic
-from PyQt6.QtWidgets import QDialog, QMainWindow, QApplication, QPushButton
+from PyQt6.QtCore import QObject, QUrl
+from PyQt6.QtWidgets import QDialog, QLabel, QMainWindow, QApplication, QWidget, QPushButton
+from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkReply, QNetworkRequest
+from PyQt6.QtWebSockets import QWebSocket
+
+DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 #Config file
-with open('config.json') as config_file:
+with open(os.path.join(DIRECTORY,'config.json')) as config_file:
     config = json.load(config_file)
 SOCKETIO = config['SOCKETIO']
 MACHINES = config['MACHINES']
-#SOCKETIO
-class SocketIO():
+#WEBSOCKET
+class WebSocketClient(QObject):
     def __init__(self):
-        pass
+        super().__init__()
+        self.websocket = QWebSocket()
+        self.websocket.connected.connect(self.on_connected)
+        self.websocket.textMessageReceived.connect(self.on_message)
+    #CONNECT FUNCTION
+    def connect(self, url):
+        self.websocket.open(QUrl(url))
+    #CONNECT EVENT
+    def on_connected(self):
+        print("Connected to WebSocket server")
+    #RECEIVED A JSON MESSAGE FROM SOCKET
+    def on_message(self, message):
+        print("Message received:", message)
+        # Handle the real-time message here, update the UI if needed
+    #SEND A JSON MESSAGE TO API
+    def send_message(self, message):
+        self.websocket.sendTextMessage(message)
 #MAIN WINDOW
 class MainWindow(QMainWindow):
     def __init__(self):
+        #UI INITIALIZATION
         super().__init__()
-        uic.loadUi("MainWindow.ui", self)
-        self.setup_windows()
-        #CONNNECT BUTTONS
-        self.MachineViewButton.clicked.connect(self.machine_view.show())
-    #INITIALIZING WINDOWS
-    def setup_windows(self):
-        self.machine_view = MachineView()
-        for machine in MACHINES:
-            window = self.find(QDialog, machine['BUTTON'])
-            if not window:
-                machine['NAME'] = SingularMachineView()
-    #BRINGS WINDOW TO FRONT
-    def show_window(self, window):
-        window.show()
-#MACHINE VIEW WINDOW
-class MachineView(QDialog):
-    def __init__(self):    
-        super().__init__()
-        uic.loadUi("MachineView.ui", self)
+        uic.loadUi(os.path.join(DIRECTORY,"MainWindow.ui"), self)
         self.setup_windows()
         self.setup_buttons()
+        #REQUESTS AND REAL TIME UPDATE SETUP
+        self.network_manager = QNetworkAccessManager(self)
+        self.network_manager.finished.connect(self.on_finished)
+        self.websocket = WebSocketClient()
+        self.websocket.connect("ws://192.168.1.153:5000")
+        self.data_points = {
+            'time_stamp': "Time Stamp: N/A",
+            'mold': "Mold: N/A",
+            'shot_count': "Shot Count: N/A",
+            'cycle_time': "Cycle Time: N/A",
+            'filling_peak': "Filling Peak: N/A",
+            'plast_time': "Plast Time: N/A",
+            'vp_switch_pos': "VP Switch Pos: N/A",
+            'min_cush_pos': "Min Cush Pos: N/A",
+            'hr_left': "Hours Left: N/A",
+            'min_left': "Minutes Left: N/A",
+            'percentage_left': "Percentage Left: N/A",
+            'shot_left': "Shots Left: N/A"}
+    #INITIALIZING WINDOWS
+    def setup_windows(self):
+        self.machine_views = {}
+        for machine in MACHINES:
+            dialog = SingularMachineView()
+            dialog.setObjectName(machine['DIALOG'])
+            dialog.MachineID.setText(machine['NAME'])
+            self.stackedWidget.addWidget(dialog)
+            self.machine_views[machine['ID']] = dialog
+    #BUTTON LINKING
     def setup_buttons(self):
+        self.pushButton.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.FloorView))
+        self.exit_button = self.findChild(QPushButton, "ButtonMainMenu")
+        self.exit_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.MainMenu))
         for machine in MACHINES:
             button = self.findChild(QPushButton, machine['BUTTON'])
             if button:
-                button.clicked.connect(lambda _, name=machine['NAME']: self.show_window(name))
-    def show_window(self, name):
-        name.show()
-#FLOOR VIEW WINDOW
-class FloorView(QDialog):
-    def __init__(self):    
-        super().__init__()
-        uic.loadUi("FloorView.ui", self)
+                dialog = self.machine_views.get(machine['ID'])
+                if dialog:
+                    button.clicked.connect(lambda _, d=dialog: self.stackedWidget.setCurrentWidget(d))
+    #GET REQUEST RESPONSE HANDLER
+    def on_finished(self, reply:QNetworkReply):
+        if reply.error() == QNetworkReply.NetworkError.NoError:
+            data = reply.readAll().data().decode()
+            print("Data received:", data)
+        else:
+            print("Error:", reply.errorString())
 #SINGLE MACHINE VIEW
 class SingularMachineView(QDialog):
     def __init__(self):    
         super().__init__()
-        uic.loadUi("SingularMachine.ui", self)
+        uic.loadUi(os.path.join(DIRECTORY,"SingularMachine.ui"), self)
+        self.ButtonExit.clicked.connect(lambda: main_window.stackedWidget.setCurrentWidget(main_window.FloorView))
 #Run the app
 app = QApplication([])
-window = MainWindow()
-window.show()
+main_window = MainWindow()
+main_window.setCentralWidget(main_window.stackedWidget)
+main_window.showMaximized()
 app.exec()
